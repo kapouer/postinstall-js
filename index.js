fee0ad8bb3de0ac0fc9fdee9b26ea8f62e546b62
Jérémy Lal <kapouer@melix.org>
2018-03-26 15:07:44 +0200

    Output source map

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const browserify = require('browserify');
+const exorcist = require('exorcist');
 const promisePipe = require('promisepipe');
 const fs = require('fs');
 
 module.exports = function(input, output, options) {
-	var tr = browserify(input).transform('bubleify', {
-	}).transform('uglifyify', {
+	var tr = browserify({
+		debug: true
 	})
+	.add(input)
+	.transform('bubleify', {})
+	.transform('uglifyify', {})
 	.bundle()
+	.pipe(exorcist(`${output}.map`))
 	.pipe(fs.createWriteStream(output));
 
 	return promisePipe(tr);
