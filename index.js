1f2605a6a13a64649b9f69ea21fa0148a81bb6e3
Jérémy Lal <kapouer@melix.org>
2019-12-02 22:13:02 +0100

    Use swc instead of babel + uglify

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
 const Cache = require('postinstall-cache');
-const presetEnv = require.resolve('@babel/preset-env');
-// const runtimePlugin = require.resolve('@babel/plugin-transform-runtime');
-// const regeneratorRuntime = require.resolve('regenerator-runtime/runtime.js');
 
 const cacheWorker = Cache.worker({
 	dirname: __dirname
 });
 
 module.exports = function(inputs, output, options) {
-	var opts = Object.assign({
-		modules: false
-	}, options);
-
-	opts.babel = {
-		presets: [
-			[presetEnv, {
-				modules: opts.modules,
-				exclude: ["@babel/plugin-transform-typeof-symbol"]
-			}]
-		],
-		// plugins: [
-			// [ runtimePlugin, {
-			// 	helpers: true,
-			// 	regenerator: false
-			// }]
-		// ],
+	var opts = Object.assign({}, options);
+	opts.transpiler = {
 		sourceMaps: false,
-		compact: false
+		minify: opts.minify !== false,
+		jsc: {
+			transform: {
+				target: "es5"
+			}
+		}
 	};
-
-	if (opts.minify !== false) {
-		opts.babel.comments = false;
-	}
-	if (opts.comments !== undefined) opts.babel.comments = opts.comments;
-	//if (!opts.modules) inputs = [regeneratorRuntime].concat(inputs);
-
 	return cacheWorker(inputs, output, opts);
 };
 
