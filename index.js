092907401e347ba121201256cc5bf6dded731ad1
Jérémy Lal <kapouer@melix.org>
2022-05-30 18:21:16 +0200

    Update babel, simplify config

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const Cache = require('postinstall-cache');
 const presetEnv = require.resolve('@babel/preset-env');
-const plugins = [
-	"@babel/plugin-proposal-class-properties",
-	"@babel/plugin-proposal-private-methods",
-	"@babel/plugin-proposal-optional-chaining"
-].map((str) => require.resolve(str));
-// const runtimePlugin = require.resolve('@babel/plugin-transform-runtime');
-// const regeneratorRuntime = require.resolve('regenerator-runtime/runtime.js');
 
 const cacheWorker = Cache.worker({
 	dirname: __dirname
@@ -27,13 +20,6 @@ module.exports = function(inputs, output, options) {
 		presets: [
 			[presetEnv, presetOpts]
 		],
-		plugins: [
-			plugins[0], plugins[1]
-			// ,[ runtimePlugin, {
-			// 	helpers: true,
-			// 	regenerator: false
-			// }]
-		],
 		sourceMaps: false,
 		compact: false
 	};
@@ -42,7 +28,6 @@ module.exports = function(inputs, output, options) {
 		opts.babel.comments = false;
 	}
 	if (opts.comments !== undefined) opts.babel.comments = opts.comments;
-	//if (!opts.modules) inputs = [regeneratorRuntime].concat(inputs);
 
 	return cacheWorker(inputs, output, opts);
 };
